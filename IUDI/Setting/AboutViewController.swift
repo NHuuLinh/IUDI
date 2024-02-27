fb49715d9e1314be7dae6dc3e92f7fb2814980e4
Nguyễn Vương Quốc <vuongquoc2002@gmail.com>
2024-02-27 16:38:52 +0700

    no message

---
+++ IUDI/Setting/AboutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AboutViewController.swift
+//  IUDI
+//
+//  Created by Quoc on 27/02/2024.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
