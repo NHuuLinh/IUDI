ff111860cdfe43e7420e8b9a6534ac0c3976e755
Nguyễn Vương Quốc <vuongquoc2002@gmail.com>
2024-03-14 16:06:48 +0700

    no message

--- IUDI/Setting/Group/InGroup/Cell/subView/subViewController.swift
+++ IUDI/Setting/Group/InGroup/Cell/subView/subViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 import Alamofire
 
-protocol FilterSettingDelegate: AnyObject{
-        func getNear()
-}
-
 class subViewController: UIViewController {
     
     var didSelectItem: (() -> Void)?
@@ -20,9 +16,7 @@ class subViewController: UIViewController {
     
     @IBOutlet weak var hiddenPost: UIButton!
     @IBOutlet weak var deletePost: UIButton!
-    
-    weak var delegate : FilterSettingDelegate?
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -38,27 +32,39 @@ class subViewController: UIViewController {
     
     func deleteData(){
         guard let postId = postId else {
-            print("khoong co post id")
+            print("khong co post id")
             return
         }
         
-        let deleteURL = "https://api.iudi.xyz/api/forum/delete_post/\(postId)"
+        let confirmationAlert = UIAlertController(title: "Xác nhận", message: "Bạn có chắc chắn muốn xóa bài đăng này không?", preferredStyle: .alert)
         
-        AF.request(deleteURL, method: .delete).validate(statusCode: 200...299).responseDecodable(of: GroupDataPosts.self){response
-            in
-            switch response.result {
-            case .success:
-                // Xóa bài đăng thành công
-                // Có thể cập nhật giao diện hoặc thực hiện hành động khác sau khi xóa
-                print("Bài đăng đã được xóa thành công")
-                self.deleteCompletion?()
-                // Gọi closure để thông báo cho controller biết rằng một bài đăng đã được xóa
-                self.didSelectItem?()
-                
-            case .failure(let error):
-                // Xảy ra lỗi khi xóa bài đăng
-                print("Lỗi khi xóa bài đăng: \(error.localizedDescription)")
+        let deleteAction = UIAlertAction(title: "Xóa", style: .destructive) { (_) in
+            let deleteURL = "https://api.iudi.xyz/api/forum/delete_post/\(postId)"
+            
+            AF.request(deleteURL, method: .delete).validate(statusCode: 200...299).responseDecodable(of: GroupDataPosts.self){response
+                in
+                switch response.result {
+                case .success:
+                    // Xóa bài đăng thành công
+                    // Có thể cập nhật giao diện hoặc thực hiện hành động khác sau khi xóa
+                    print("Bài đăng đã được xóa thành công")
+                    self.deleteCompletion?()
+                    // Gọi closure để thông báo cho controller biết rằng một bài đăng đã được xóa
+                    self.didSelectItem?()
+                    
+                case .failure(let error):
+                    // Xảy ra lỗi khi xóa bài đăng
+                    print("Lỗi khi xóa bài đăng: \(error.localizedDescription)")
+                }
             }
         }
+        
+        let cancelAction = UIAlertAction(title: "Hủy", style: .cancel)
+        
+        confirmationAlert.addAction(deleteAction)
+        confirmationAlert.addAction(cancelAction)
+        
+        present(confirmationAlert, animated: true, completion: nil)
     }
+
 }
